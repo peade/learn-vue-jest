f7714059b4312b63500b7ddf57a0c75c6966d984
lhf <faheliu@163.com>
2020-06-29 14:11:05 +0800

    单元测试初步

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,4 +1,23 @@
 // 校验手机号码
+/**
+ *
+ *
+ * @export
+ * @param { string } phone
+ * @returns boolean
+ */
 export function CheckPhone(phone) {
-  return /^1[3-9]\d{9}$/.test(phone);
+  return /^1[3-9]\d{9}$/.test(phone)
+}
+
+/**
+ *
+ *
+ * @export
+ * @param { number } a
+ * @param { number } b
+ * @returns number
+ */
+export function AddNumber(a, b) {
+  return a + b
 }
--- tests/unit/util.spec.js
+++
@@ -1,7 +0,0 @@
-import { CheckPhone } from "@/utils/tools";
-
-describe("CheckPhone", () => {
-  it("check phon enumber when passed", () => {
-    expect(CheckPhone("13512344321")).toBe(true);
-  });
-});
