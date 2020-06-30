c654afbbc1f2d16b88720206127dcffeb2c0845c
lhf <faheliu@163.com>
2020-06-30 13:44:42 +0800

    axios 请求单元测试

--- tests/unit/mock_module.spec.js
+++ tests/unit/mock_module.spec.js
@@ -8,3 +8,14 @@ test('aaa', () => {
   const res = foo()
   expect(res).not.toBe(0)
 })
+
+import axios from 'axios'
+import Users from '@/utils/user'
+jest.mock('axios')
+
+test('should fetch users', () => {
+  const users = [{ name: 'bob' }]
+  const resp = { data: users }
+  axios.get.mockResolvedValue(resp)
+  return Users.all().then(data => expect(data).toEqual(resp))
+})
