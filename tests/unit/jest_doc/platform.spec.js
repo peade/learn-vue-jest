a26ab45b4313c396ae674e1835f084b28a2eafb2
lhf <faheliu@163.com>
2020-07-02 13:25:25 +0800

    clean platform.spec.js

--- tests/unit/jest_doc/platform.spec.js
+++ tests/unit/jest_doc/platform.spec.js
@@ -1,36 +1,36 @@
-const { getChangedFilesForRoots } = require('jest-changed-files')
+// const { getChangedFilesForRoots } = require('jest-changed-files')
 
 // 打印出当前目录最后修改过的一组文件
-getChangedFilesForRoots(['./'], {
-  lastCommit: true
-}).then(result => console.log(result.changedFiles))
+// getChangedFilesForRoots(['./'], {
+//   lastCommit: true
+// }).then(result => console.log(result.changedFiles))
 
 const diff = require('jest-diff')
 
 const a = { a: { b: { c: 5 } } }
 const b = { a: { b: { c: 6 } } }
-
-const result = diff(a, b)
+diff(a, b)
+// const result = diff(a, b)
 
 // print diff
-console.log(result)
+// console.log(result)
 
-const { parseWithComments } = require('jest-docblock')
+// const { parseWithComments } = require('jest-docblock')
 
-const code = `
-/**
- * This is a sample
- *
- * @flow
- */
+// const code = `
+// /**
+//  * This is a sample
+//  *
+//  * @flow
+//  */
 
- console.log('Hello World!');
-`
+//  console.log('Hello World!');
+// `
 
-const parsed = parseWithComments(code)
+// const parsed = parseWithComments(code)
 
 // prints an object with two attributes: comments and pragmas.
-console.log(parsed)
+// console.log(parsed)
 
 const { validate } = require('jest-validate')
 
@@ -38,14 +38,14 @@ const configByUser = {
   transform: '<rootDir>/node_modules/my-custom-transform'
 }
 
-const resultVali = validate(configByUser, {
+validate(configByUser, {
   comment: '  Documentation: http://custom-docs.com',
   exampleConfig: { transform: '<rootDir>/node_modules/babel-jest' }
 })
 
-console.log(resultVali)
+// console.log(resultVali)
 
-const prettyFormat = require('pretty-format')
+// const prettyFormat = require('pretty-format')
 
 const val = { object: {} }
 val.circularReference = val
@@ -53,7 +53,7 @@ val[Symbol('foo')] = 'foo'
 val.map = new Map([['prop', 'value']])
 val.array = [-0, Infinity, NaN]
 
-console.log(prettyFormat(val))
+// console.log(prettyFormat(val))
 
 test('must be one', () => {
   expect(1).toBe(1)
