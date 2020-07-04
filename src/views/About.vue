628c7b4d3d99557a62629d88326fac12cfb559cb
peade <peade@live.cn>
2020-07-04 19:08:47 +0800

    调整项目代码格式化

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>{{ id }},{{ name }}</div>
   </div>
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        id: '',
+        name: ''
+      }
+    },
+    mounted() {
+      this.id = this.$route.params.id
+      this.name = this.$route.query.name
+    }
+  }
+</script>
