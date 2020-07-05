8c4a2ccfb7a200d8609152cd48db13ba3f4e349b
peade <peade@live.cn>
2020-07-05 17:36:52 +0800

    vuex test

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import mutations from './mutations'
+import getters from './getters'
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
+export const storeConfig = {
+  state: {
+    count: 0
+  },
+  mutations,
+  getters,
   actions: {},
   modules: {}
-})
+}
+
+export default new Vuex.Store(storeConfig)
